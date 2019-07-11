6a765b54d26f27ee35fb6c54835776984e42cf5f
Beagler <beagler@runbox.com>
2019-07-11 10:42:37 -0400

    skeleton

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express() 
+
+app.get('', (req, res) =>{
+    res.send("hello express")
+});
+
+app.get('/help', (req, res) =>{
+    res.send("help page")
+}); 
+
+app.get('/about', (req, res) =>{
+    res.send("about page")
+}); 
+
+app.get('/weather', (req, res) =>{
+    res.send("weather page")
+}); 
+
+app.listen(3000, () =>{
+    console.info('server is on port 3000')
+})
