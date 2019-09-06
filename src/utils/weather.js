53dca69000cafc0f38879769c8aa7ce3766c97f5
= <=>
2019-09-05 23:39:16 -0400

    fixed precip

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -22,9 +22,12 @@ function getWeather (latitude, longitude, cb) {
       return cb(MESSAGES.location, undefined)
     } else {
       const current = response.body.currently
-      const today = response.body.daily.data[0]
-      const msg = `${today.summary} It is currently ${Math.round(current.temperature)}F degrees with a high expected of ${Math.round(today.temperatureHigh)}F. 
-      There is a ${current.precipProbability < 1 ? '<1' : Math.round(current.precipProbability)}% chance of precipitation.
+      const today = response.body.daily.data[0];
+	const precip = Math.round(current.precipProbability * 100);
+	const alerts = response.body.alerts[0];
+      const msg = `${today.summary} It is currently ${current.summary} and ${Math.round(current.temperature)}F degrees with a high expected of ${Math.round(today.temperatureHigh)}F. 
+      There is a ${precip}% chance of precipitation.
+      \n\n${alerts ? alerts.title : ``}
       `
       return cb(undefined, msg)
     }
