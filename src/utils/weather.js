9f973efd38d702fff816828f91c57d90055182d5
= <=>
2019-09-06 00:14:36 -0400

    alert fix

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -24,10 +24,10 @@ function getWeather (latitude, longitude, cb) {
       const current = response.body.currently
       const today = response.body.daily.data[0];
 	const precip = Math.round(current.precipProbability * 100);
-	const alerts = response.body.alerts[0];
+	const alerts = response.body.alerts;
       const msg = `${today.summary} It is currently ${current.summary} and ${Math.round(current.temperature)}F degrees with a high expected of ${Math.round(today.temperatureHigh)}F. 
       There is a ${precip}% chance of precipitation.
-      \n\n${alerts ? alerts.title : ``}
+      \n\n${alerts ? alerts[0].title : ``}
       `
       return cb(undefined, msg)
     }
